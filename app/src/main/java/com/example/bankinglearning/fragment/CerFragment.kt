29ac1aa67408d10218f207adbc7a06419d54c41e
정하경 <hkjhk3626@cau.ac.kr>
2022-07-18 15:26:13 +0900

    feat: online banking learning app 7/18 second commit

---
+++ app/src/main/java/com/example/bankinglearning/fragment/CerFragment.kt
@@ -0,0 +1,46 @@
+package com.example.bankinglearning.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.bankinglearning.R
+import kotlinx.android.synthetic.main.fragment_cer.*
+
+class CerFragment : Fragment() {
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cer, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+
+        btn_home.setOnClickListener {
+            navController.navigate(R.id.action_cerFragment_to_mainFragment)
+        }
+
+        btn_back1.setOnClickListener {
+            navController.navigate(R.id.action_cerFragment_to_mainFragment)
+        }
+        btn_red1.setOnClickListener {
+            navController.navigate(R.id.action_cerFragment_to_cer1Fragment)
+        }
+
+
+    }
+
+}
